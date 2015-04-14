f74fda9e80bc562cd440577909a4e9a91de0012e
Amir <amirc6@gmail.com>
2015-04-14 22:46:48 +0300

    finish css hw3

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
 		md2html: {
 			one_file: {
 				options: {},
@@ -10,8 +11,44 @@ module.exports = function (grunt) {
 					dest: 'outline.html'
 				}]
 			}	
+		},
+		
+		connect: {
+			all: {
+				options: {
+					port: 80,
+					base: '',
+					livereload: true,
+					keepalive: false,
+
+					middleware: function(connect, options) {
+						return [
+						require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
+						connect.static(options.base[0])
+						];
+					}
+				}
+			}
+		},
+
+		watch: {
+			js: {
+				files: ['js/**/*.js'],
+				tasks: ['livereload']
+			},
+			css: {
+				files: ['css/**/*.css'],
+				tasks:['livereload']
+			},
+			html: {
+				files: ['**/*.html'],
+				tasks:['livereload']
+			}
 		}
 	});
 	
+	grunt.registerTask('default', ['connect', 'watch']);
 	grunt.loadNpmTasks('grunt-md2html');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 };
